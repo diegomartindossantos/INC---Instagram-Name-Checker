07272ab95d0c19607e277c0f3e8464e4ad5dbf05
Diego Martín dos Santos <martindossantosdiego@gmail.com>
2020-07-04 00:51:40 +0200

    -Añadido una funcion de comprobación de usuario. -Añadida una lista de usuarios de ejemplo para comprobar. -Añadido un ciclo for para recorrer la lista mediante la funcion de comprobacion de usuario.

--- inc.py
+++ inc.py
@@ -1,5 +1,29 @@
 import requests
 
-instagram_url = "https://www.instagram.com/"
 
-r = requests.get(instagram_url)
+#Creacion de VARIABLES para su posterior uso
+instagram_url = "https://www.instagram.com/"
+test_users = ["leomessi","cristiano","zz910832hj"]
+
+def try_user(user):
+    #Definiendo urls
+    url = instagram_url+user
+    r = requests.get(url)
+    #comienza la comprobacion del status_code de la petición
+    if r.status_code == 200:
+        print (f"[TAKEN] {user}")
+    elif r.status_code == 404:
+        print (f"[FREE] {user}")
+    else:
+        print (f"[ERROR] {user}")
+
+
+
+for x in test_users:
+    try_user(x)
+
+
+
+#Cosas pendientes: 
+#PROXIES HEADERS, TIEMPO ENTRE SOLICITUDES, COMPROBACIÓN DE LAS STR DE LOS NOMBRES
+
