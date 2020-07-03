ef3c802473997df849a16e094daf1b2fecfce70e
Diego Martín dos Santos <martindossantosdiego@gmail.com>
2020-07-04 01:15:17 +0200

    -Añadida comprobación de la string del usuario:     -Comprobación de si contiene un salto de linea.     -Comprobación de si contiene espaciado. -Editada la lista de usuarios para que fuerze al programa a encontrar errores de string y corregirlos.

--- inc.py
+++ inc.py
@@ -1,9 +1,10 @@
 import requests
+from time import sleep
 
 
 #Creacion de VARIABLES para su posterior uso
 instagram_url = "https://www.instagram.com/"
-test_users = ["leomessi","cristiano","zz910832hj"]
+test_users = ["leomessi\n","cristiano ","zz910832hj"] #El salto de linea y el espacio estan a proposito para forzar la comprobacion
 
 def try_user(user):
     #Definiendo urls
@@ -19,11 +20,26 @@ def try_user(user):
 
 
 
-for x in test_users:
-    try_user(x)
+for x in test_users: #POR CADA USUARIO...
+    if "\n" in x:  #Comprobamos que el usuario no tiene un salto de linea en la string que le guarda. (Al cargar archivos puede pasar)
+        x = x.replace("\n","\\n")
+        print (f"[ERROR] Fixing lane break error from user: -{x}-") #Replace \n por \\n para poder imprimir el usuario sin un salto de linea en consola
+        x = x.replace("\\n","")
+        sleep(1)
+        print (f"[FIXED] -{x}-")
+        print ("#"*50)
+        try_user(x)
+    elif " " in x: #Comprobamos que el usuario no contiene espacios.
+        print (f"[ERROR] Fixing SPACE error from user -{x}-")
+        x = x.replace(" ","")
+        print (f"[FIXED] -{x}-")
+        print ("#"*50)
+        try_user(x)
+    else: #Si la str del usuario esta limpia...
+        try_user(x)
 
 
 
 #Cosas pendientes: 
-#PROXIES HEADERS, TIEMPO ENTRE SOLICITUDES, COMPROBACIÓN DE LAS STR DE LOS NOMBRES (errores de lectura)
+#PROXIES HEADERS, TIEMPO ENTRE SOLICITUDES
 
