6cf99b2da74d0580634ea8a97a5f8dab5ef395ce
Diego Martín dos Santos <martindossantosdiego@gmail.com>
2020-07-04 01:33:21 +0200

    -Eliminadas algunas línea inútiles.
    -Añadidas más medidas de omprobación de usuarios:
        -Comprobación de si la string contiene carácteres especiales.
        -Comprobación de si la string está vacía.

--- inc.py
+++ inc.py
@@ -1,10 +1,13 @@
 import requests
 from time import sleep
+import re
 
 
 #Creacion de VARIABLES para su posterior uso
 instagram_url = "https://www.instagram.com/"
-test_users = ["leomessi\n","cristiano ","zz910832hj"] #El salto de linea y el espacio estan a proposito para forzar la comprobacion
+test_users = ["leomessi\n","cristiano ","zz910832hj","test|@#\|","bbcnews","antena3com"] #El salto de linea y el espacio estan a proposito para forzar la comprobacion
+regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
 
 def try_user(user):
     #Definiendo urls
@@ -23,18 +26,19 @@ def try_user(user):
 for x in test_users: #POR CADA USUARIO...
     if "\n" in x:  #Comprobamos que el usuario no tiene un salto de linea en la string que le guarda. (Al cargar archivos puede pasar)
         x = x.replace("\n","\\n")
-        print (f"[ERROR] Fixing lane break error from user: -{x}-") #Replace \n por \\n para poder imprimir el usuario sin un salto de linea en consola
+        print (f"[ERROR - AUTOFIXED] Fixing lane break error from user: -{x}-") #Replace \n por \\n para poder imprimir el usuario sin un salto de linea en consola
         x = x.replace("\\n","")
-        sleep(1)
-        print (f"[FIXED] -{x}-")
-        print ("#"*50)
         try_user(x)
     elif " " in x: #Comprobamos que el usuario no contiene espacios.
-        print (f"[ERROR] Fixing SPACE error from user -{x}-")
+        print (f"[ERROR - AUTOFIXED] Fixing SPACE error from user -{x}-")
         x = x.replace(" ","")
-        print (f"[FIXED] -{x}-")
-        print ("#"*50)
         try_user(x)
+    elif x == "":
+        print ("[ERROR - PASSING] User Empty.")
+        pass
+    elif (regex.search(x) != None):
+        print (f"[ERROR - PASSING] Special characters in user -{x}-")
+        pass
     else: #Si la str del usuario esta limpia...
         try_user(x)
 
